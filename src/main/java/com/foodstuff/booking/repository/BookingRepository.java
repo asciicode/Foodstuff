b9e66c29cc1b58480d898702b546313eb0058f00
Allen Catadman <allen.catadman@monash.edu>
2022-10-27 11:44:27 +1100

    remove query

--- src/main/java/com/foodstuff/booking/repository/BookingRepository.java
+++ src/main/java/com/foodstuff/booking/repository/BookingRepository.java
@@ -9,11 +9,9 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> findByDayOrderByTime(int day);
 
-    @Query(value = "SELECT e FROM Booking as e WHERE (e.day = ?1) and (e.time = ?2) "
-    )
     List<Booking> findByDayAndTime(int day, int time);
+
     List<Booking> findByUserIdOrderByDayAscTimeAsc(int userId);
-    @Query(value = "SELECT count(e.id) FROM Booking as e WHERE (e.userId = ?1) and  (e.day = ?2) and (e.time = ?3) "
-    )
+
     long countByUserIdAndDayAndTime(int userId, int day, int time);
 }
